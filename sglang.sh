7dca4ef1a0f9412aeb0f14d0c58dc371697047b7
_dimweb_ <dimweb.tech@mail.ru>
2025-04-28 00:24:36 +0000

    add generation for sglang

--- sglang.sh
+++ sglang.sh
@@ -1,7 +1,7 @@
 volume=$PWD/data
 model=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
-# docker run -ti --rm --gpus all \
-docker run -d -ti --rm --gpus all \
+# docker run -d -ti --rm --gpus all \
+docker run -ti --rm --gpus all \
     --shm-size 1g \
     -p 1337:30000 \
     -v $volume:/root/.cache/huggingface \
@@ -9,5 +9,7 @@ docker run -d -ti --rm --gpus all \
     -w /code \
     --ipc=host \
     lmsysorg/sglang:v0.4.5-cu125 \
-    python3 -m sglang.launch_server --model-path $model --host 0.0.0.0 --port 30000
+    python3 -m sglang.launch_server --model-path $model --host 0.0.0.0 --port 30000 --attention-backend triton --sampling-backend pytorch --disable-cuda-graph --disable-cuda-graph-padding --disable-custom-all-reduce --disable-mla --random-seed 42 --max-running-request 1
+    # python3 -m sglang.launch_server --model-path $model --host 0.0.0.0 --port 30000 
+    # --max-running-request 1
     # python3 -m sglang.launch_server --model-path $model --host 0.0.0.0 --port 30000 --dp-size 4
